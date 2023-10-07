c8d0e5a3de1e35eafe5d9f0f063a8e01393aae64
ludin-lee <alsdyd0331@naver.com>
2023-10-08 07:47:02 +0900

    FEAT:SCHOOL_02

--- src/controllers/school.controller.js
+++ src/controllers/school.controller.js
@@ -6,12 +6,12 @@ export default class SchoolController {
     const { name, region } = req.body;
     const { tokenFor } = req.locals.decodedToken;
 
-    try {
-      if (tokenFor !== USER_TYPE.admin) {
-        res.status(403).json({ result: false, message: "Permission Denied" });
-        return;
-      }
+    if (tokenFor !== USER_TYPE.admin) {
+      res.status(403).json({ result: false, message: "Permission Denied" });
+      return;
+    }
 
+    try {
       const schoolInfo = await db.school.create({
         name,
         region,
@@ -31,7 +31,29 @@ export default class SchoolController {
     }
   };
   getSchoolList = async (req, res) => {
+    const { tokenFor, id } = req.locals.decodedToken;
+
+    if (tokenFor !== USER_TYPE.student) {
+      res.status(403).json({ result: false, message: "Permission Denied" });
+      return;
+    }
+
     try {
+      const schoolList = await db.studentSchoolMapping.findAll({
+        where: { studentId: id },
+        attributes: [
+          [db.sequelize.col("schoolId_schools.id"), "id"],
+          [db.sequelize.col("schoolId_schools.name"), "name"],
+          [db.sequelize.col("schoolId_schools.region"), "region"],
+        ],
+        include: {
+          model: db.school,
+          as: "schoolId_schools",
+          attributes: [],
+        },
+      });
+
+      res.status(200).json({ result: true, data: { schoolList } });
     } catch (err) {
       console.error(err);
       res.status(err.status || 500).json({
