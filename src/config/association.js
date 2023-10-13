8786b1d2385f96fedf8f39d2cb695b581550e501
ludin-lee <alsdyd0331@naver.com>
2023-10-13 14:36:03 +0900

    FIX:DB_01

--- src/config/association.js
+++ src/config/association.js
@@ -1,36 +1,36 @@
 export default function associationAll(db) {
   // school-news 1:N관계
-  db.school.belongsTo(db.news, {
-    as: "schoolId_news",
+  db.news.belongsTo(db.school, {
+    as: "school",
     foreignKey: "schoolId",
     onDelete: "cascade",
   });
-  db.news.hasMany(db.school, {
-    as: "schools",
-    foreignKey: "friendId",
+  db.school.hasMany(db.news, {
+    as: "news",
+    foreignKey: "schoolId",
     onDelete: "cascade",
   });
 
   //studentSchoolMapping-student 1:N 관계
-  db.studentSchoolMapping.belongsTo(db.student, {
-    as: "studentId_studnets",
+  db.student.belongsTo(db.studentSchoolMapping, {
+    as: "studentSchoolMapping",
     foreignKey: "studentId",
     onDelete: "cascade",
   });
-  db.student.hasMany(db.studentSchoolMapping, {
-    as: "studentSchoolMappings",
+  db.studentSchoolMapping.hasMany(db.student, {
+    as: "students",
     foreignKey: "studentId",
     onDelete: "cascade",
   });
 
   //studentSchoolMapping-school 1:N 관계
-  db.studentSchoolMapping.belongsTo(db.school, {
-    as: "schoolId_schools",
+  db.school.belongsTo(db.studentSchoolMapping, {
+    as: "studentSchoolMapping",
     foreignKey: "schoolId",
     onDelete: "cascade",
   });
-  db.school.hasMany(db.studentSchoolMapping, {
-    as: "studentSchoolMappings",
+  db.studentSchoolMapping.hasMany(db.school, {
+    as: "schools",
     foreignKey: "schoolId",
     onDelete: "cascade",
   });
--- src/config/sync.js
+++ src/config/sync.js
@@ -3,11 +3,11 @@ import db from "../models/index.js";
 
 await db.sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
 
-// await db.admin.sync({ alter: true });
-// await db.news.sync({ alter: true });
-// await db.school.sync({ alter: true });
-// await db.student.sync({ alter: true });
-// await db.studentSchoolMapping.sync({ alter: true });
+await db.admin.sync({ alter: true });
+await db.news.sync({ alter: true });
+await db.school.sync({ alter: true });
+await db.student.sync({ alter: true });
+await db.studentSchoolMapping.sync({ alter: true });
 
 await db.sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
 await db.sequelize.close();
